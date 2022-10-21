a898940e88a17e3497387306822c871d211a1bef
Adam Funk <a.funk@ducksburg.com>
2022-10-21 12:09:53 +0100

    CLI for debugging the preprocessor

---
+++ src/debug_img.py
@@ -0,0 +1,43 @@
+import argparse
+from path import Path
+import locations
+from dataloader_iam import DataLoaderIAM, Batch
+from preprocessor import Preprocessor
+
+
+def parse_args() -> argparse.Namespace:
+    """Parses arguments from the command line."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_dir',
+                        help='Directory containing IAM dataset.',
+                        type=Path,
+                        required=True)
+    parser.add_argument('--debug-dir',
+                        help='directory to dump CV2',
+                        type=Path,
+                        required=True)
+    parser.add_argument('--batch_size', help='Batch size.', type=int, default=100)
+
+    return parser.parse_args()
+
+
+def main():
+    # parse arguments and set model location and CTC decoder
+    args = parse_args()
+    locations.set_model_dir(args.model_dir)
+    locations.set_debug_dir(args.debug_dir)
+
+    loader = DataLoaderIAM(args.data_dir, args.batch_size, fast=False)
+    line_mode = False
+    preprocessor = Preprocessor(main.get_img_size(line_mode), data_augmentation=True, line_mode=line_mode)
+
+    while loader.has_next():
+        iter_info = loader.get_iterator_info()
+        batch = loader.get_next()
+        preprocessor.process_batch(batch)
+        print(f'Batch: {iter_info[0]}/{iter_info[1]}')
+
+
+if __name__ == '__main__':
+    main()
